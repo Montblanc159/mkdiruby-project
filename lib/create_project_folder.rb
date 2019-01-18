c70ebe410fc8f373f7acbccba3a995f90c60e2f8
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-18 16:47:20 +0100

    Initial commit

---
+++ lib/create_project_folder.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal : true
+
+require 'colorize'
+
+def check_if_user_gave_input
+  abort("mkdir: missing input") if ARGV.empty?
+end
+
+def ruby_file_names
+  file_name = ARGV
+end
+
+def create_folders
+  Dir.mkdir("./lib")
+  Dir.mkdir("./spec")
+end
+
+def create_files
+  ruby_file_names.each do |ruby_file|
+    system("touch lib/#{ruby_file}.rb")
+    file = File.open("./lib/#{ruby_file}.rb", "w")
+    file.puts("# frozen_string_literal : true")
+    file.puts("")
+    file.puts("require 'pry'")
+    file.puts("require 'dotenv'")
+    file.puts("")
+    file.puts("Dotenv.load")
+    file.close
+
+    system("touch spec/#{ruby_file}_spec.rb")
+    file = File.open("./spec/#{ruby_file}_spec.rb", "w")
+    file.puts("# frozen_string_literal : true")
+    file.puts("")
+    file.puts("require_relative '../lib/#{ruby_file}.rb'")
+    file.close
+  end
+  system("touch .env")
+  file = File.open(".env")
+  file.close
+
+  system("touch .gitignore")
+  file = File.open(".gitignore", "w")
+  file.puts(".env")
+  file.close
+
+  system("touch README.md")
+  file = File.open("README.md", "w")
+  file.puts("#")
+  file.puts("")
+  file.puts("**Ceci est un projet ruby**")
+  file.puts("")
+  file.puts("Réalisé par")
+  file.close
+end
+
+def create_gem_file
+
+  system("touch Gemfile")
+  file = File.open("Gemfile", "w")
+  file.puts('source "https://rubygems.org"')
+  file.puts("ruby '2.5.1'")
+  file.puts("gem 'rubocop', '~> 0.57.2'")
+  file.puts("gem 'rspec'")
+  file.puts("gem 'pry'")
+  file.close
+end
+
+def term_inits
+  system("rspec --init")
+  system("git init")
+  system("bundle install")
+  system("git add .")
+
+  puts "Copy the adress of the main remote git :".colorize("blue")
+  print "> "
+  ARGV.clear
+  git_adress = gets.chomp
+
+  system("git remote add origin #{git_adress}")
+  system("atom .")
+end
+
+def perform
+  check_if_user_gave_input
+  create_folders
+  create_gem_file
+  create_files
+  term_inits
+end
+
+perform
